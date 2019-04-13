65bb2f02f0a1d70d73f2bcde5a572446e7954d2b
ankur <ankurgarg2312@gmail.com>
2019-04-13 17:58:39 +0530

    Partially complete Home screen and successfully hit verify otp api

--- app/src/main/java/com/klickfie/MainActivity.java
+++ app/src/main/java/com/klickfie/MainActivity.java
@@ -5,8 +5,10 @@ import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.klickfie.utilities.Constants;
+import com.klickfie.utilities.SharedPref;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +24,13 @@ public class MainActivity extends AppCompatActivity {
         hand.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intentLogin=new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intentLogin);
+                if(!SharedPref.getInstance().getBoolean(Constants.Login_ID)) {
+                    Intent intentLogin = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intentLogin);
+                }
+                else{
+                    startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                }
                 finish();
             }
         }, Constants.SPLASH_TIME);
